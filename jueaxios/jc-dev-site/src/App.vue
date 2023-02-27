fe3e21238b6bc7424873f31aa7a62ed8527bec70
jose-cleiton <cleitons835@gmail.com>
2023-02-27 15:28:28 -0300

    feat: :sparkles: Footer

--- jueaxios/jc-dev-site/src/App.vue
+++ jueaxios/jc-dev-site/src/App.vue
@@ -3,6 +3,7 @@
 <div id="app">
   <AppHeader/>
   <Home/>
+  <Footer/>
 </div>
  
 </template>
---
+++ jueaxios/jc-dev-site/src/components/footer/indexFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    
+  </footer>
+
+
+</template>
+
+<script src="./Footer.js"></script>
+
+<style src="./Footer.css" scoped></style>
