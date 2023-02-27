0deba032abb738591e6af8fbb8f8a7f30912e1e4
jose-cleiton <cleitons835@gmail.com>
2023-02-27 17:03:59 -0300

    feat: :sparkles: definindo a rota "/"

---
+++ jueaxios/jc-dev-site/src/routes/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/pages/home/indexHome.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
